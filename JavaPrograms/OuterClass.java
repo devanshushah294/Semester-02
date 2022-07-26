938777419a08989820b05f2904b25271d32dcc10
Devanshu Shah <93487920+devanshushah294@users.noreply.github.com>
2022-07-26 16:21:41 +0530

    Java Programs

---
+++ JavaPrograms/OuterClass.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class OuterClass {
+
+    String outerField = "Outer field";
+    static String staticOuterField = "Static outer field";
+
+    class InnerClass {
+        void accessMembers() {
+            System.out.println(outerField);
+            System.out.println(staticOuterField);
+        }
+    }
+
+    static class StaticNestedClass {
+        void accessMembers(OuterClass outer) {
+            // Compiler error: Cannot make a static reference to the non-static
+            //     field outerField
+            // System.out.println(outerField);
+            System.out.println(outer.outerField);
+            System.out.println(staticOuterField);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Inner class:");
+        System.out.println("------------");
+        OuterClass outerObject = new OuterClass();
+        OuterClass.InnerClass innerObject = outerObject.new InnerClass();
+        innerObject.accessMembers();
+
+        System.out.println("\nStatic nested class:");
+        System.out.println("--------------------");
+        StaticNestedClass staticNestedObject = new StaticNestedClass();        
+        staticNestedObject.accessMembers(outerObject);
+        
+        System.out.println("\nTop-level class:");
+        System.out.println("--------------------");
+        StaticNestedClass topLevelObject = new StaticNestedClass();        
+        topLevelObject.accessMembers(outerObject);                
+    }
+}
